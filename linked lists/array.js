0f16c13a17924474b24c350f97f1c454686b5f79
Derek S <dschwan21@me.com>
2019-12-24 23:20:46 -0800

    added array

---
+++ linked lists/array.js
@@ -0,0 +1,36 @@
+class MyArray {
+    constructor() {
+        this.array = [];
+    }
+
+    add(data) {
+        this.array.push(data);
+    }
+
+    remove(data) {
+        this.array = this.array.filter(current => current !== data);
+    }
+
+    search(data) {
+        const foundIndex = this.array.indexOf(data);
+        if(~foundIndex) {
+            return foundIndex;
+        }
+
+        return null;
+    }
+
+    getAtIndex(index) {
+        return this.array[index];
+    }
+
+    length() {
+        return this.array.length;
+    }
+
+    print() {
+        console.log(this.array.join(' '));
+    }
+}
+
+const array = new MyArray();
