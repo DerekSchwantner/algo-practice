18bfaa6e9ed8c507cd282d9d0cdf01e5d8644121
Derek S <dschwan21@me.com>
2020-01-10 21:13:38 -0800

    practiced with queues

--- stacks and ques/Practice.js
+++ stacks and ques/Practice.js
@@ -1,26 +1,57 @@
-class Queue {
+function Node(data) {
+        this.value = data
+        this.next = null
+}
+
+class LinkedList {
     constructor() {
-        this.storage = []
+        this.head = null
+        this.tail = null
+        this.length = 0
     }
 
-    enqueue(item){
-        this.storage.push(item)
+    add(data) {
+        if (!this.head) {
+            this.head = new Node(data)
+            this.tail = this.head
+            this.length++
+        } else if (this.head && !this.tail) {
+            this.tail = new Node(data)
+            this.head.next = this.tail
+            this.length++
+        } else {
+            this.tail.next = new Node(data)
+            this.tail = this.tail.next
+            this.length++
+        }
     }
 
-    dequeue(){
-        this.storage.shift()
-    }
+    remove(data) {
+        let currentNode = this.head
 
-    peek(){
-        return this.storage
+        while (currentNode){
+            if (currentNode === this.head && currentNode.value === data){
+                this.head = this.head.next
+            } else if (currentNode.next === this.tail && this.tail.value === data){
+                currentNode.next = null
+                this.tail = currentNode
+            } else if (currentNode.next.value === data){
+                currentNode.next = currentNode.next.next
+            } else {
+                currentNode = currentNode.next
+            }
+        }
     }
 }
 
+let list = new LinkedList()
 
-const q = new Queue()
+list.add(5)
+list.add(6)
+list.add(7)
+list.add(8)
+list.add(9)
+// list.remove(6)
+// list.remove()
 
-q.enqueue(7)
-q.enqueue(8)
-q.enqueue(9)
-
-console.log(q.storage)
+console.log(list)
