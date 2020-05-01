8854af2c60844c552f703c6f7f85286ba7147912
Derek S <dschwan21@me.com>
2020-04-30 22:59:04 -0700

    added python algo for generating all permutations of an array

---
+++ Practice Questions/recursion/permutations.py
@@ -0,0 +1,12 @@
+# generate all the permutations of an array
+
+def perm(a, k=0):
+   if k == len(a):
+      print a
+   else:
+      for i in xrange(k, len(a)):
+         a[k], a[i] = a[i] ,a[k]
+         perm(a, k+1)
+         a[k], a[i] = a[i], a[k]
+
+perm([1,2,3])
